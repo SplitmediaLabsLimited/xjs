e41dc6f10e5d022c6dc65fe34c30c48f2d8f04d0
Michael Ybanez <miyb@splitmedialabs.com>
2015-07-23 11:55:56 +0800

    feat: implement application property setter and getter functions

--- src/internal/app.ts
+++ src/internal/app.ts
@@ -1,13 +1,24 @@
 /// <reference path="../../defs/_references.ts" />
 
+import exec from './internal';
+
+var POSTMESSAGE_CLOSE: string = '1';
+var POSTMESSAGE_SIZE: string = '2';
+
 export class App {
   /** Get the value of the given property */
   static get(name: string): Promise<string> {
-    return Promise.resolve(''); 
+    return new Promise(resolve => {
+      exec('AppGetPropertyAsync', name, resolve);
+    });
   }
 
   /** Sets the value of a property */
   static set(name: string, value: string): Promise<Boolean> {
-    return Promise.resolve(true);
+    return new Promise(resolve => {
+        exec('AppSetPropertyAsync', name, value, ret => {
+          resolve(ret === '0' ? false : true);
+        });
+    });
   }
 }
