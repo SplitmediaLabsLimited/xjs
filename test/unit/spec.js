3bd52e830951222792ab06bbf734772a6550edf3
Michael Ybanez <miyb@splitmedialabs.com>
2015-07-24 18:24:46 +0800

    test: sample failing test case to test Travis integration

--- test/unit/spec.js
+++ test/unit/spec.js
@@ -1,17 +1,23 @@
 /* this file should contain a Jasmine spec for unit testing */
 
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
+describe('A suite', function() {
+  it('contains spec with an expectation', function() {
     expect(true).toBe(true);
   });
 });
 
-describe("A suite is just a function", function() {
+describe('A suite is just a function', function() {
   var a;
 
-  it("and so is a spec", function() {
+  it('and so is a spec', function() {
     a = true;
 
     expect(a).toBe(true);
+  });
+});
+
+describe('A suite that fails', function() {
+  it('is a spec that fails', function() {
+    expect(false).toBe(true);
   });
 });
