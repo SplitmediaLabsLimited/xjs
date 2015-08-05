ac62c0b7af75b629a30dbcfc61a54b427e4537a0
Michael Ybanez <miyb@splitmedialabs.com>
2015-08-05 20:25:59 +0800

    test: add type-testing for audio device methods

--- test/unit/specs/system/audio.js
+++ test/unit/specs/system/audio.js
@@ -19,7 +19,7 @@ describe('Audio', function() {
 
               setTimeout(function() {
                 window.OnAsyncCallback(randomNumber, '<list><dev name="Speakers (XSplit  Stream  Audio  Renderer)" adapter="XSplit  Stream  Audio  Renderer" adapterdev="Speakers" id="{0.0.0.00000000}.{95611a5d-84a5-4838-b5bd-4344bef36632}" DataFlow="Render" State="Active" DSoundGuid="{95611A5D-84A5-4838-B5BD-4344BEF36632}" WaveId="1"/><dev name="Microphone (HD Webcam C615)" adapter="HD Webcam C615" adapterdev="Microphone" id="{0.0.1.00000000}.{b7709bea-527e-4f60-afb0-cd36431972ad}" DataFlow="Capture" State="Active" DSoundGuid="{B7709BEA-527E-4F60-AFB0-CD36431972AD}" WaveId="1"/><dev name="Internal AUX Jack (NVIDIA High Definition Audio)" adapter="NVIDIA High Definition Audio" adapterdev="Internal AUX Jack" id="{0.0.1.00000000}.{edb3bf9a-bd62-43c2-8c3f-1b57d3cc602a}" DataFlow="Capture" State="Not present" DSoundGuid="{EDB3BF9A-BD62-43C2-8C3F-1B57D3CC602A}" WaveId=""/><dev name="Stereo Mix (Realtek High Definition Audio)" adapter="Realtek High Definition Audio" adapterdev="Stereo Mix" id="{0.0.1.00000000}.{fd0f34df-d9e8-4de9-a212-dfdc9cb75178}" DataFlow="Capture" State="Disabled" DSoundGuid="{FD0F34DF-D9E8-4DE9-A212-DFDC9CB75178}" WaveId=""/></list>');
-              },10);
+              }, 10);
             }
           });
       }
@@ -48,6 +48,44 @@ describe('Audio', function() {
         for (var i = devices.length - 1; i >= 0; i--) {
           expect(devices[i].name.toLowerCase().indexOf('xsplit')).toBe(-1);
         }
+      });
+    });
+  });
+
+  describe('system Audio', function() {
+    var promise;
+
+    beforeEach(function() {
+      promise = System.getAudioDevices();
+      if (!/xsplit broadcaster/ig.test(navigator.appVersion)) {
+        spyOn(window.external, 'AppGetPropertyAsync')
+          .and.callFake(function(funcName) {
+            if(funcName === 'wasapienum') {
+              var randomNumber=Math.floor(Math.random()*1000);
+
+              setTimeout(function() {
+                window.OnAsyncCallback(randomNumber, '<list><dev name="Microphone (HD Webcam C615)" adapter="HD Webcam C615" adapterdev="Microphone" id="{0.0.1.00000000}.{b7709bea-527e-4f60-afb0-cd36431972ad}" DataFlow="Capture" State="Active" DSoundGuid="{B7709BEA-527E-4F60-AFB0-CD36431972AD}" WaveId="1"/></list>');
+              }, 10);
+            }
+          });
+      }
+    });
+
+    it('should have a string ID', function() {
+      promise.then(function(devices) {
+        expect(devices[0].getId()).toBeInstanceOf(String);
+      });
+    });
+
+    it('should have a string name', function() {
+      promise.then(function(devices) {
+        expect(devices[0].getName()).toBeInstanceOf(String);
+      });
+    });
+
+    it('should have a string dataflow', function() {
+      promise.then(function(devices) {
+        expect(devices[0].getDataFlow()).toBeInstanceOf(String);
       });
     });
   });
