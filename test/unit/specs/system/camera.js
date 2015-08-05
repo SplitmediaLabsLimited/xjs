b78c63ffe3436f66e7121536ec88c271ef684014
Michael Ybanez <miyb@splitmedialabs.com>
2015-08-05 21:26:47 +0800

    test: type-checking for camera device objects

--- test/unit/specs/system/camera.js
+++ test/unit/specs/system/camera.js
@@ -1,10 +1,10 @@
-/* globals describe, it, expect, require */
+/* globals describe, it, expect, require, beforeEach, spyOn */
 
 describe('Camera', function() {
   'use strict';
 
   var XJS = require('xjs');
-  var System = xjs.System;
+  var System = XJS.System;
 
   describe('should list camera devices', function() {
     var promise;
@@ -18,7 +18,7 @@ describe('Camera', function() {
               var randomNumber=Math.floor(Math.random()*1000);
 
               setTimeout(function() {
-                window.OnAsyncCallback(randomNumber, '<list><dev disp="clsid:{39F50F4C-99E1-464A-B6F9-D605B4FB5918}" name="Elgato Game Capture HD"/><dev disp="\@device:pnp:\\\\?\\usb\#vid_046d&amp;pid_082c&amp;mi_02\#6&amp;37c59c5d&amp;0&amp;0002\#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global" name="HD Webcam C615"/><dev disp="\@device:sw:{860BB310-5D01-11D0-BD3B-00A0C911CE86}\\{VHSplitProc}_XSplitBroadcaster_1_staticsource_VIDEO" name="XSplitBroadcaster"/></list>');
+                window.OnAsyncCallback(randomNumber, '<list><dev disp="clsid:{39F50F4C-99E1-464A-B6F9-D605B4FB5918}" name="Elgato Game Capture HD"/><dev disp="@device:pnp:\\\\?\\usb#vid_046d&amp;pid_082c&amp;mi_02#6&amp;37c59c5d&amp;0&amp;0002#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global" name="HD Webcam C615"/><dev disp="@device:sw:{860BB310-5D01-11D0-BD3B-00A0C911CE86}\\{VHSplitProc}_XSplitBroadcaster_1_staticsource_VIDEO" name="XSplitBroadcaster"/></list>');
               }, 10);
             }
           });
@@ -48,6 +48,38 @@ describe('Camera', function() {
         for (var i = devices.length - 1; i >= 0; i--) {
           expect(devices[i].name.toLowerCase().indexOf('xsplit')).toBe(-1);
         }
+      });
+    });
+  });
+
+  describe('system camera devices', function() {
+    var promise;
+
+    beforeEach(function() {
+      promise = System.getAudioDevices();
+      if (!/xsplit broadcaster/ig.test(navigator.appVersion)) {
+        spyOn(window.external, 'AppGetPropertyAsync')
+          .and.callFake(function(funcName) {
+            if(funcName === 'wasapienum') {
+              var randomNumber=Math.floor(Math.random()*1000);
+
+              setTimeout(function() {
+                window.OnAsyncCallback(randomNumber, '<list><dev disp="@device:pnp:\\\\?\\usb#vid_046d&amp;pid_082c&amp;mi_02#6&amp;37c59c5d&amp;0&amp;0002#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global" name="HD Webcam C615"/></list>');
+              }, 10);
+            }
+          });
+      }
+    });
+
+    it('should have a string name', function() {
+      promise.then(function(devices) {
+        expect(devices[0].getId()).toBeTypeOf(String);
+      });
+    });
+
+    it('should have a string friendly name', function() {
+      promise.then(function(devices) {
+        expect(devices[0].getId()).toBeTypeOf(String);
       });
     });
   });
