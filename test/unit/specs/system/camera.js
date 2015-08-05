4e0e5f79a8fd874ec615a34bdaf775b6a434d15f
Michael Ybanez <miyb@splitmedialabs.com>
2015-08-05 21:19:29 +0800

    test: add mocking for camera devices, and correct method matcher usage

--- test/unit/specs/system/camera.js
+++ test/unit/specs/system/camera.js
@@ -3,11 +3,27 @@
 describe('Camera', function() {
   'use strict';
 
-  var xjs = require('xjs');
+  var XJS = require('xjs');
   var System = xjs.System;
 
   describe('should list camera devices', function() {
     var promise;
+
+    beforeEach(function() {
+      promise = System.getAudioDevices();
+      if (!/xsplit broadcaster/ig.test(navigator.appVersion)) {
+        spyOn(window.external, 'AppGetPropertyAsync')
+          .and.callFake(function(funcName) {
+            if(funcName === 'wasapienum') {
+              var randomNumber=Math.floor(Math.random()*1000);
+
+              setTimeout(function() {
+                window.OnAsyncCallback(randomNumber, '<list><dev disp="clsid:{39F50F4C-99E1-464A-B6F9-D605B4FB5918}" name="Elgato Game Capture HD"/><dev disp="\@device:pnp:\\\\?\\usb\#vid_046d&amp;pid_082c&amp;mi_02\#6&amp;37c59c5d&amp;0&amp;0002\#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global" name="HD Webcam C615"/><dev disp="\@device:sw:{860BB310-5D01-11D0-BD3B-00A0C911CE86}\\{VHSplitProc}_XSplitBroadcaster_1_staticsource_VIDEO" name="XSplitBroadcaster"/></list>');
+              }, 10);
+            }
+          });
+      }
+    });
 
     it('through a promise', function() {
       promise = System.getCameraDevices();
@@ -22,8 +38,8 @@ describe('Camera', function() {
 
     it('with correct properties', function() {
       promise.then(function(devices) {
-        expect(devices).eachHasProperties('disp');
-        expect(devices).eachHasProperties('name');
+        expect(devices)
+          .eachHasMethods(['getName', 'getFriendlyName'].join(','));
       });
     });
 
