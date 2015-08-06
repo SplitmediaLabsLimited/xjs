ed4208e8df7feede1887aafa4165f8eaeaadaeca
dcefram <me@danielcefram.com>
2015-08-06 17:07:49 +0800

    test: update test cases to support async testing

--- test/unit/specs/system/camera.js
+++ test/unit/specs/system/camera.js
@@ -1,4 +1,4 @@
-/* globals describe, it, expect, require, beforeEach, spyOn */
+/* globals describe, it, expect, require, beforeAll, spyOn */
 
 describe('Camera', function() {
   'use strict';
@@ -10,7 +10,6 @@ describe('Camera', function() {
     var promise;
 
     beforeEach(function() {
-      promise = System.getCameraDevices();
       if (!/xsplit broadcaster/ig.test(navigator.appVersion)) {
         spyOn(window.external, 'AppGetPropertyAsync')
           .and.callFake(function(funcName) {
@@ -20,9 +19,12 @@ describe('Camera', function() {
               setTimeout(function() {
                 window.OnAsyncCallback(randomNumber, '<list><dev disp="clsid:{39F50F4C-99E1-464A-B6F9-D605B4FB5918}" name="Elgato Game Capture HD"/><dev disp="@device:pnp:\\\\?\\usb#vid_046d&amp;pid_082c&amp;mi_02#6&amp;37c59c5d&amp;0&amp;0002#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global" name="HD Webcam C615"/><dev disp="@device:sw:{860BB310-5D01-11D0-BD3B-00A0C911CE86}\\{VHSplitProc}_XSplitBroadcaster_1_staticsource_VIDEO" name="XSplitBroadcaster"/></list>');
               }, 10);
+
+              return randomNumber;
             }
           });
       }
+      promise = System.getCameraDevices();
     });
 
     it('through a promise', function() {
@@ -30,24 +32,27 @@ describe('Camera', function() {
       expect(promise).toBeInstanceOf(Promise);
     });
 
-    it('as an array', function() {
+    it('as an array', function(done) {
       promise.then(function(devices) {
         expect(devices).toBeInstanceOf(Array);
+        done();
       });
     });
 
-    it('with correct properties', function() {
+    it('with correct properties', function(done) {
       promise.then(function(devices) {
         expect(devices)
           .eachHasMethods(['getName', 'getId'].join(','));
+          done();
       });
     });
 
-   it('excludes XSplit camera devices', function() {
+   it('excludes XSplit camera devices', function(done) {
       promise.then(function(devices) {
         for (var i = devices.length - 1; i >= 0; i--) {
           expect(devices[i].getName().toLowerCase().indexOf('xsplit')).toBe(-1);
         }
+        done();
       });
     });
   });
@@ -56,7 +61,6 @@ describe('Camera', function() {
     var promise;
 
     beforeEach(function() {
-      promise = System.getCameraDevices();
       if (!/xsplit broadcaster/ig.test(navigator.appVersion)) {
         spyOn(window.external, 'AppGetPropertyAsync')
           .and.callFake(function(funcName) {
@@ -66,20 +70,25 @@ describe('Camera', function() {
               setTimeout(function() {
                 window.OnAsyncCallback(randomNumber, '<list><dev disp="@device:pnp:\\\\?\\usb#vid_046d&amp;pid_082c&amp;mi_02#6&amp;37c59c5d&amp;0&amp;0002#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global" name="HD Webcam C615"/></list>');
               }, 10);
+
+              return randomNumber;
             }
           });
       }
+      promise = System.getCameraDevices();
     });
 
-    it('should have a string id', function() {
+    it('should have a string id', function(done) {
       promise.then(function(devices) {
         expect(devices[0].getId()).toBeTypeOf('string');
+        done();
       });
     });
 
-    it('should have a string name', function() {
+    it('should have a string name', function(done) {
       promise.then(function(devices) {
         expect(devices[0].getName()).toBeTypeOf('string');
+        done();
       });
     });
   });
