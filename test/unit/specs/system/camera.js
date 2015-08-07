827b93c5d3ca11891f0ae3b03fe978ee61ca990e
SML-MeSo <meso@splitmedialabs.com>
2015-08-07 16:04:23 +0800

    test: include testing for exclusion of Tridef Smart Cam

--- test/unit/specs/system/camera.js
+++ test/unit/specs/system/camera.js
@@ -17,7 +17,19 @@ describe('Camera', function() {
               var randomNumber=Math.floor(Math.random()*1000);
 
               setTimeout(function() {
-                window.OnAsyncCallback(randomNumber, '<list><dev disp="clsid:{39F50F4C-99E1-464A-B6F9-D605B4FB5918}" name="Elgato Game Capture HD"/><dev disp="@device:pnp:\\\\?\\usb#vid_046d&amp;pid_082c&amp;mi_02#6&amp;37c59c5d&amp;0&amp;0002#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global" name="HD Webcam C615"/><dev disp="@device:sw:{860BB310-5D01-11D0-BD3B-00A0C911CE86}\\{VHSplitProc}_XSplitBroadcaster_1_staticsource_VIDEO" name="XSplitBroadcaster"/></list>');
+                window.OnAsyncCallback(randomNumber, '<list>' +
+                  '<dev disp="@device:sw:{860BB310-5D01-11D0-BD3B-00A0C911CE86}' +
+                  '\\{778ABFB2-E87B-48A2-8D33-675150FCF8A2}"' +
+                  ' name="TriDef SmartCam"/>' +
+                  '<dev disp="clsid:{39F50F4C-99E1-464A-B6F9-D605B4FB5918}"' +
+                  ' name="Elgato Game Capture HD"/>' +
+                  '<dev disp="@device:pnp:\\\\?\\usb#vid_046d&amp;pid_082c' +
+                  '&amp;mi_02#6&amp;37c59c5d&amp;0&amp;0002#' +
+                  '{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global"' +
+                  ' name="HD Webcam C615"/>' +
+                  '<dev disp="@device:sw:{860BB310-5D01-11D0-BD3B-00A0C911CE86}' +
+                  '\\{VHSplitProc}_XSplitBroadcaster_1_staticsource_VIDEO"' +
+                  ' name="XSplitBroadcaster"/></list>');
               }, 10);
 
               return randomNumber;
@@ -47,10 +59,16 @@ describe('Camera', function() {
       });
     });
 
-   it('excludes XSplit camera devices', function(done) {
+   it('excludes XSplit camera devices and Tridef Smart Cam', function(done) {
       promise.then(function(devices) {
         for (var i = devices.length - 1; i >= 0; i--) {
           expect(devices[i].getName().toLowerCase().indexOf('xsplit')).toBe(-1);
+          expect(devices[i].getId().toLowerCase())
+            .not.toEqual(("@DEVICE:SW:{860BB310-5D01-11D0-BD3B-00A0C911CE86}\\" + 
+            "{778abfb2-e87b-48a2-8d33-675150fcf8a2}").toLowerCase());
+          var id = devices[i].getId().toLowerCase();
+          var idComp = ("@DEVICE:SW:{860BB310-5D01-11D0-BD3B-00A0C911CE86}\\" +
+            "{778abfb2-e87b-48a2-8d33-675150fcf8a2}").toLowerCase();
         }
         done();
       });
@@ -68,7 +86,11 @@ describe('Camera', function() {
               var randomNumber=Math.floor(Math.random()*1000);
 
               setTimeout(function() {
-                window.OnAsyncCallback(randomNumber, '<list><dev disp="@device:pnp:\\\\?\\usb#vid_046d&amp;pid_082c&amp;mi_02#6&amp;37c59c5d&amp;0&amp;0002#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global" name="HD Webcam C615"/></list>');
+                window.OnAsyncCallback(randomNumber, '<list>' +
+                  '<dev disp="@device:pnp:\\\\?\\usb#vid_046d&amp;pid_082c' +
+                  '&amp;mi_02#6&amp;37c59c5d&amp;0&amp;0002#' +
+                  '{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global"' +
+                  ' name="HD Webcam C615"/></list>');
               }, 10);
 
               return randomNumber;
