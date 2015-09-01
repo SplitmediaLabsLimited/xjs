2502beb35eb94224d4eb789783073171ebd1f88b
Michael Ybanez <miyb@splitmedialabs.com>
2015-09-01 22:18:26 +0800

    fix: wrong checking for camera item and audio item types

--- src/core/scene.ts
+++ src/core/scene.ts
@@ -362,13 +362,13 @@ export class Scene {
       if (Number(jsonArr[index]['type']) === ItemTypes.GAMESOURCE) {
         typeResolve(new GameItem(jsonArr[index]));
         } else if (Number(jsonArr[index]['type']) === ItemTypes.LIVE &&
-          jsonArr[index]['item'].substring(
+          jsonArr[index]['item'].indexOf(
             '{33D9A762-90C8-11D0-BD43-00A0C911CE86}') === -1) {
             typeResolve(new CameraItem(jsonArr[index]));
         } else if (Number(jsonArr[index]['type']) === ItemTypes.LIVE &&
-          jsonArr[index]['item'].substring(
+          jsonArr[index]['item'].indexOf(
             '{33D9A762-90C8-11D0-BD43-00A0C911CE86}') !== -1) {
-      typeResolve(new AudioItem(jsonArr[index]));
+            typeResolve(new AudioItem(jsonArr[index]));
         }
         else {
             typeResolve(new Item(jsonArr[index]));
