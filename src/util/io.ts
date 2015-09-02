b67010fe56446084578d78326c36902a1122f3b8
Michael Ybanez <miyb@splitmedialabs.com>
2015-09-02 18:55:06 +0800

    feat: add openFileDialog call for IO class

--- src/util/io.ts
+++ src/util/io.ts
@@ -28,7 +28,7 @@ export class IO {
    */
   static getWebContent(url: string): Promise<string> {
     return new Promise(resolve => {
-      exec('GetWebContent', url, (encoded) => {
+      exec('GetWebContent', url, encoded => {
         resolve(encoded);
       });
     });
@@ -39,5 +39,69 @@ export class IO {
    */
   static openUrl(url: string) {
     exec('OpenUrl', url);
+  }
+
+  private static _ALLOW_MULTI_SELECT: number = 0x200;
+  private static _FILE_MUST_EXIST: number = 0x1000;
+  private static _FORCE_SHOW_HIDDEN: number = 0x10000000;
+
+  /**
+   * Opens a file dialog for the user to select a file (or multiple files).
+   * Returns an array of strings, each of which contains the full path
+   * and filename of a selected file. Rejects when the dialog is canceled.
+   *
+   * The first (optional) argument is a JSON object that can be used to indicate
+   * that certain flags should be true. These are documented as follows:
+   * - `allowMultiSelect`: allows users to select multiple files.
+   * - `fileMustExist`: prevents users from typing a name of a nonexistent file
+   * - `forceShowHidden`: lets the dialog show files marked as System or Hidden
+   *  (but not both)
+   *
+   * The second argument (also optional) is a JSON object used to specify the
+   * filter for items to be displayed. It takes two members:
+   * - `name`: the description of the filter (for example: Image Files)
+   * - `extensions`: an array of file extensions (for example: `['jpg','bmp']`);
+   */
+  static openFileDialog(optionBag ?: {
+        allowMultiSelect ?: boolean,
+        fileMustExist    ?: boolean,
+        forceShowHidden  ?: boolean
+      },
+      filter ?: { name : boolean, extensions : String[] }): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      let flags: number = 0;
+      if (optionBag !== undefined && optionBag !== null) {
+        if (optionBag.allowMultiSelect === true) {
+          flags = flags | IO._ALLOW_MULTI_SELECT;
+        }
+
+        if (optionBag.fileMustExist === true) {
+          flags = flags | IO._FILE_MUST_EXIST;
+        }
+
+        if (optionBag.forceShowHidden === true) {
+          flags = flags | IO._FORCE_SHOW_HIDDEN;
+        }
+      }
+
+      let filterString: string = '';
+      if (filter !== undefined && filter !== null &&
+          filter.name !== undefined && filter.extensions !== undefined) {
+        filterString = filter.name + '|';
+        filterString += (filter.extensions.map(val => {
+          return '*.' + val;
+        })).join(';');
+        filterString += '||';
+      }
+
+      exec('OpenFileDialogAsync', null, null, String(flags), filterString,
+          path => {
+            if (path !== "null") {
+              resolve(path.split('|'));
+            } else {
+              reject(Error('File selection cancelled.'));
+            }
+      });
+    });
   }
 }
