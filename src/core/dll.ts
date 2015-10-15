937d821d0f2d8472e5d540090dba420007850ab7
Michael Ybanez <mikefyba@gmail.com>
2015-10-15 14:48:27 +0800

    fix: correct usage of DLLs
    Closes #21.

--- src/core/dll.ts
+++ src/core/dll.ts
@@ -7,24 +7,19 @@ import {exec} from '../internal/internal';
 export class Dll {
 
   /**
-   *  param: (funcName: string, ...params: any[])
-   *  return: _any_ (see DLL documentation)
+   *  param: (funcName: string, ...params: string[])
+   *  return: string (see DLL documentation)
    *
-   *  Calls a function from a DLL. Note that function name collisions may be
-   *  possible so ideally your DLLs should have namespaced function names;
-   *  otherwise, the first DLL to be found containing the function name will be
+   *  Calls a function from a DLL. Note that function name collisions are
+   *  possible. The first DLL to be found containing the function name will be
    *  called.
    *
-   *  You can add any number of additional parameters. If your DLL call requires
-   *  a callback, use that as the final parameter of this function.
-   *
-   *  The return value will depend on the DLL call. Most synchronous calls will
-   *  return a string value immediately, while those with callbacks may simply
-   *  return an integer identifier for the callback.
+   *  Some DLLs have callbacks. Assign a handler function to that callback in
+   *  the global namespace, and the DLL will call that function accordingly.
    *
    *  See the documentation of your specific DLL for more details.
    */
-  static callFunction(func: string, ...params: any[]): string {
+  static callFunction(func: string, ...params: string[]): string {
       params.unshift(func);
       params.unshift('CallDll');
       return exec.apply(this, params);
