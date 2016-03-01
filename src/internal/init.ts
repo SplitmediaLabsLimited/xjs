f9ca61697ddfa006a3fed53b043022b1b6ca8d4e
dcefram <me@danielcefram.com>
2016-03-01 21:40:31 +0800

    fix: lock source id if environment is config window
    Fixes #74

--- src/internal/init.ts
+++ src/internal/init.ts
@@ -91,7 +91,7 @@ function getCurrentSourceId(): Promise<any> {
           let id = decodeURIComponent(result);
           Item.setBaseId(id);
 
-          if (Environment.isSourcePlugin()) {
+          if (Environment.isSourcePlugin() || Environment.isSourceConfig()) {
             Item.lockSourceSlot(id);
           }
           resolve();
