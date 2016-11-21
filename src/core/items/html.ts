0043d01db961a371d8e5dfab49a0f7586b708255
ViAb@SML <viab@splitmedialabs.com>
2016-11-21 20:16:57 +0800

    docs: link comments of item methods shared with source

--- src/core/items/html.ts
+++ src/core/items/html.ts
@@ -120,164 +120,74 @@ export class HtmlItem extends Item implements IItemLayout, IItemColor,
   }
 
   //iHtmlSource
+
   /**
-   * return: Promise<boolean>
-   *
-   * Check if browser is rendered transparent
+   * See: {@link #core/HtmlSource#isBrowserTransparent isBrowserTransparent}
    */
   isBrowserTransparent: () => Promise<boolean>
 
   /**
-   * param: Promise<boolean>
-   * ```
-   * return: Promise<HtmlSource>
-   * ```
-   *
-   * Enable or disabled transparency of CEF browser
-   *
-   * *Chainable.*
+   * See: {@link #core/HtmlSource#getBrowserCustomSize getBrowserCustomSize}
    */
   enableBrowserTransparency: (value: boolean) => Promise<HtmlItem>
 
   /**
-   * return: Promise<Rectangle>
-   *
-   * Gets the custom browser window size (in pixels) for the item, if set,
-   * regardless of its layout on the mixer. Returns a (0, 0) Rectangle if no
-   * custom size has been set.
-   *
-   * See also: {@link #util/Rectangle Util/Rectangle}
+   * See: {@link #core/HtmlSource#getBrowserCustomSize getBrowserCustomSize}
    */
   getBrowserCustomSize: () => Promise<Rectangle>
 
   /**
-   * param: Promise<Rectangle>
-   * ```
-   * return: Promise<IHtmlItem>
-   * ```
-   *
-   * Sets the custom browser window size for the item
-   * regardless of its layout on the mixer
-   *
-   * *Chainable.*
-   *
-   * See also: {@link #util/Rectangle Util/Rectangle}
+   * See: {@link #core/HtmlSource#setBrowserCustomSize setBrowserCustomSize}
    */
   setBrowserCustomSize: (value: Rectangle) => Promise<HtmlItem>
 
   /**
-   * return: Promise<boolean>
-   *
-   * Check if right click events are sent to the item or not.
-   *
-   * #### Usage
-   *
-   * ```javascript
-   * item.getAllowRightClick().then(function(isRightClickAllowed) {
-   *   // The rest of your code here
-   * });
-   * ```
+   * See: {@link #core/HtmlSource#getAllowRightClick getAllowRightClick}
    */
   getAllowRightClick: () => Promise<boolean>
 
   /**
-   * param: (value:boolean)
-   * ```
-   * return: Promise<Source>
-   * ```
-   *
-   * Allow or disallow right click events to be sent to the item. Note that
-   * you can only catch right click events using `mouseup/mousedown`
-   *
-   * *Chainable*
-   *
-   * #### Usage
-   *
-   * ```javascript
-   * item.setAllowRightClick(true).then(function(item) {
-   *   // Promise resolves with the same Item instance
-   * });
-   * ```
+   * See: {@link #core/HtmlSource#setAllowRightClick setAllowRightClick}
    */
   setAllowRightClick: (value: boolean) => Promise<HtmlItem>
 
   /**
-   * return: Promise<string>
-   *
-   * Gets the javascript commands to be executed on item upon load
+   * See: {@link #core/HtmlSource#getBrowserJS getBrowserJS}
    */
   getBrowserJS: () => Promise<string>
 
   /**
-   * param: (js: string, refresh: boolean = false)
-   * ```
-   * return: Promise<IHtmlItem>
-   * ```
-   *
-   * Sets the javascript commands to be executed on item
-   * right upon setting and on load. Optionally set second parameter
-   * to true to refresh item (needed to clean previously executed JS code.)
-   *
-   * *Chainable.*
+   * See: {@link #core/HtmlSource#setBrowserJS setBrowserJS}
    */
   setBrowserJS: () => Promise<HtmlItem>
 
-    /**
-   * return: Promise<boolean>
-   *
-   * Gets if BrowserJS is enabled and executed on load
+  /**
+   * See: {@link #core/HtmlSource#isBrowserJSEnabled isBrowserJSEnabled}
    */
   isBrowserJSEnabled: () => Promise<boolean>
 
   /**
-   * param: (value: boolean)
-   * ```
-   * return: Promise<IiHtmlSource>
-   * ```
-   *
-   * Enables or disables execution of the set BrowserJs upon load.
-   * Note that disabling this will require item to be refreshed
-   * in order to remove any BrowserJS previously executed.
-   *
-   * *Chainable.*
+   * See: {@link #core/HtmlSource#enableBrowserJS enableBrowserJS}
    */
   enableBrowserJS: (value: boolean) => Promise<HtmlItem>
 
   /**
-   * return: Promise<string>
-   *
-   * Gets the custom CSS applied to the document upon loading
+   * See: {@link #core/HtmlSource#getCustomCSS getCustomCSS}
    */
   getCustomCSS: () => Promise<string>
 
   /**
-   * param: (value: string)
-   * ```
-   * return: Promise<IiHtmlItem>
-   * ```
-   *
-   * Sets the custom CSS to be applied to the document upon loading
-   *
-   * *Chainable.*
+   * See: {@link #core/HtmlSource#setCustomCSS setCustomCSS}
    */
   setCustomCSS: (value: string) => Promise<HtmlItem>
 
   /**
-   * return: Promise<boolean>
-   *
-   * Gets if custom CSS is enabled and applied to the document on load
+   * See: {@link #core/HtmlSource#isCustomCSSEnabled isCustomCSSEnabled}
    */
   isCustomCSSEnabled: () => Promise<boolean>
 
   /**
-   * param: (value: boolean)
-   * ```
-   * return: Promise<IiHtmlItem>
-   * ```
-   *
-   * Enables or disables application of custom CSS to the document
-   *
-   * *Chainable.*
+   * See: {@link #core/HtmlSource#enableCustomCSS enableCustomCSS}
    */
   enableCustomCSS: (value: boolean) => Promise<HtmlItem>
 
