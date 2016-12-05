8a3576e424ed3ba450dcfd6aca48ccea6cb8f067
SML-MeSo <meso@splitmedialabs.com>
2016-12-05 22:18:08 +0800

    fix: check for presence of file parameter in static getVideoDuration method of IO class as it may result to unresolved calls

--- src/util/io.ts
+++ src/util/io.ts
@@ -122,7 +122,7 @@ export class IO {
             if (path !== 'null') {
               resolve(path.split('|'));
             } else {
-              reject(Error('File selection cancelled.'));
+              reject(new Error('File selection cancelled.'));
             }
       });
     });
@@ -139,15 +139,17 @@ export class IO {
 
   static _callback = {};
   static getVideoDuration(file: string) {
-
     return new Promise((resolve, reject) => {
+      if (typeof file !== 'undefined') {
+        if (IO._callback[file] === undefined){
+          IO._callback[file] = [];
+        }
 
-      if (IO._callback[file] === undefined){
-        IO._callback[file] = [];
+        IO._callback[file].push({resolve,reject});
+        exec('GetVideoDuration', file);
+      } else {
+        reject(new Error('No file indicated.'))
       }
-
-      IO._callback[file].push({resolve,reject});
-      exec('GetVideoDuration', file);
     });
   };
 }
