f224a7c322adbc94ff5f68c4b7c6024e7db587a6
SML-MeSo <meso@splitmedialabs.com>
2016-12-07 15:49:12 +0800

    test: polyfill String startsWith method

--- test/unit/external.js
+++ test/unit/external.js
@@ -513,3 +513,10 @@
 		window.external = MockExternal;
 
 })();
+
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
