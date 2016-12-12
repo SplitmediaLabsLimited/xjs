06a7b0109ba0c3c3815586e0115ba267189fa571
ViAb@SML <viab@splitmedialabs.com>
2016-12-12 16:40:52 +0800

    docs: update paths for some exports

--- modules/core.ts
+++ modules/core.ts
@@ -33,3 +33,9 @@ export {IItemLayout} from '../src/core/items/ilayout';
 export {IItemTransition} from '../src/core/items/itransition';
 export {IItemEffect} from '../src/core/items/ieffects';
 
+export {ActionAfterPlayback} from '../src/core/source/iplayback';
+export {CuePoint} from '../src/core/source/cuepoint';
+export {IAudio} from '../src/core/source/iaudio';
+export {ISourceConfigurable} from '../src/core/source/iconfig';
+export {ISourcePlayback} from '../src/core/source/iplayback';
+
