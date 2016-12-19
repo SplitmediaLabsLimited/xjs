8e83db0c89a584b0cdd1994a3842cffae0636d27
SML-MeSo <meso@splitmedialabs.com>
2016-12-19 17:09:21 +0800

    test: allow Mixin function to accept more than 2 functions to be looped

--- test/unit/mixin.js
+++ test/unit/mixin.js
@@ -1,3 +1,9 @@
+function PromiseLoop(condition, action, value) {
+  if (!condition(value)) return Promise.resolve();
+  return action(value).then(nextVal => PromiseLoop(condition, action, nextVal));
+}
+
+
 window.Mixin = function(fc) {
   this.fcs = [];
   if (fc !== undefined) this.register(fc);
@@ -14,15 +20,21 @@ Mixin.prototype.register = function(fc) {
 Mixin.prototype.exec = function(cond) {
   var self = this;
   return new Promise(function(done) {
-    self.fcs.map(function(fc) {
+    var funcs = self.fcs.map(function(fc) {
       return function() {
         fc();
         return new Promise(function(next) {
           cond(next);
         });
       };
-    }).reduce(function(current, next) {
-      return current().then(next);
-    }).then(done);
+    });
+
+    PromiseLoop(
+      (idx) => idx < funcs.length,
+      (idx) => funcs[idx]().then(() => Promise.resolve(++idx)),
+      0
+    ).then(() => {
+      done();
+    });
   });
 };
