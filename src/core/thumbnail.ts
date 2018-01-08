9c7f42b28a748bf59030928e05b0bd9c0ed20c54
ViAb@SML <viab@splitmedialabs.com>
2018-01-08 21:31:01 +0800

    fix: update Thumbnail class to remove other rendering methods used for a different class refactor: uppate codes to use xjs 2.6.0 scene methods

--- src/core/thumbnail.ts
+++ src/core/thumbnail.ts
@@ -2,12 +2,10 @@
 
 import {Scene} from './scene';
 import {App as iApp} from '../internal/app';
-import {Render} from '../internal/render';
-import { exec } from '../internal/internal';
 
 export class Thumbnail {
   /**
-   * param?: scene<number|Scene>
+   * param?: scene<id|Scene|undefined>
    * ```
    * return: Promise<string>
    * ```
@@ -30,83 +28,35 @@ export class Thumbnail {
     return new Promise((resolve, reject) => {
       scenePromise = new Promise(innerResolve => {
         if (scene instanceof Scene) {
-          scene.getSceneNumber()
-          .then(num => innerResolve(num - 1))
+          scene.getSceneUid()
+          .then(sceneUid => innerResolve(sceneUid))
         } else if (typeof scene === 'number') {
           if (scene < 0) {
             reject(Error('Invalid parameters. Valid range is 0 or higher'))
           } else {
-            innerResolve(scene)
+            Scene.getBySceneIndex(scene).then(curScene => {
+              return curScene.getSceneUid()
+            }).then(sceneUid => {
+              innerResolve(sceneUid)
+            })
           }
         } else if (!scene) {
-          Scene.getActiveScene().then(currScene => {
-            return currScene.getSceneNumber() //replace with getSceneIndex
-          }).then(sceneNum => {
-            innerResolve(sceneNum - 1)
+          Scene.getActiveScene().then(curScene => {
+            return curScene.getSceneUid() //replace with getSceneIndex
+          }).then(sceneUid => {
+            innerResolve(sceneUid)
           })
         } else {
           reject(Error('Invalid parameters. Valid parameter is scene or scene index'))
         }
       })
 
-      scenePromise.then(finalScene => {
-        iApp.get(`presetthumbnail:${String(finalScene)}`)
+      scenePromise.then(sceneUid => {
+        iApp.get(`presetthumbnail:${sceneUid}`)
         .then(thumb => {
           resolve(thumb)
         })
       })
     })
   }
-
-
-  // Assign a canvas and create necessary webgl context
-  static setTargetCanvas(canvas: HTMLCanvasElement,
-                         fps: number = 30): Promise<any> {
-    return new Promise((resolve, reject) => {
-      if (canvas) {
-        Render.setCanvas(canvas, fps).then(res => {
-          resolve(res)
-        })
-      } else {
-        reject(Error('No canvas selected'))
-      }
-    })
-  }
-
-  // Uses prepared canvas to dupworkspace
-  // reject if no canvas was set
-  // static renderSceneToTexture(scene: number) {
-  //   return new Promise((resolve, reject) => {
-  //     Render.drawToTexture(scene)
-  //     resolve()
-  //   })
-  // }
-
-  // If there's an active render, set rendering to true or false
-  static toggleRender(state?: boolean) {
-    return new Promise(resolve => {
-      Render.startStopRender(state).then(res => {
-        resolve(res)
-      })
-    })
-  }
-  // static setRenderFPS(fps: number) {
-  //   return new Promise(resolve => {
-  //     Render.setFPS(fps).then(() => {
-  //       resolve(true)
-  //     })
-  //   })
-  // }
-
-
-
-  static testCanvasToUse(canvasIndex, sceneId) {
-    return new Promise(resolve => {
-      Render.drawToTexture(canvasIndex, sceneId).then(res => {
-        resolve()
-      })
-    })
-  }
-
-
 }
