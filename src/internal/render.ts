f6c8c15c223eddddfa4e89620a8eb5070abdecea
ViAb@SML <viab@splitmedialabs.com>
2018-02-09 21:53:30 +0800

    fix: set initWebGL as promise to properly assign the webgl context in the array with the same index as the canvas - incorrect argument on startStopRender

--- src/internal/render.ts
+++ src/internal/render.ts
@@ -48,7 +48,7 @@ export class Render {
   // core-related variables
   private static fpsInterval = 1000 / 30;
 
-
+  // Call main function and immediately render
   static drawToTexture(canvasIndex, id, type) {
     return new Promise(resolve => {
       Render._type[canvasIndex] = type;
@@ -61,71 +61,75 @@ export class Render {
     })
   }
 
+  // intialize canvas
+  // prepare the canvas for rendering
   static initializeCanvas(thisCanvas, fps?) {
     return new Promise(resolve => {
       Render.canvases.push(thisCanvas);
       const thisIndex = Render.canvases.indexOf(thisCanvas);
-      Render.gls[thisIndex] = Render.initWebGL(thisIndex);
+      Render.initWebGL(thisIndex).then(gl => {
+        Render.gls[thisIndex] = gl;
 
-      Render.gls[thisIndex].viewport(0, 0, thisCanvas.width, thisCanvas.height);
-      Render.gls[thisIndex].clearColor(0.0, 0.0, 0.0, 1.0);
-      Render.gls[thisIndex].clearDepth(1.0);
+        Render.gls[thisIndex].viewport(0, 0, thisCanvas.width, thisCanvas.height);
+        Render.gls[thisIndex].clearColor(0.0, 0.0, 0.0, 1.0);
+        Render.gls[thisIndex].clearDepth(1.0);
 
-      let vShader = Render.gls[thisIndex].createShader(Render.gls[thisIndex].VERTEX_SHADER);
-      Render.gls[thisIndex].shaderSource(vShader, Render.vertexShaderCode);
-      Render.gls[thisIndex].compileShader(vShader);
-      if (!Render.gls[thisIndex].getShaderParameter(vShader, Render.gls[thisIndex].COMPILE_STATUS)) {
-        throw 'could not compile shader:' + Render.gls[thisIndex].getShaderInfoLog(vShader);
-      }
+        let vShader = Render.gls[thisIndex].createShader(Render.gls[thisIndex].VERTEX_SHADER);
+        Render.gls[thisIndex].shaderSource(vShader, Render.vertexShaderCode);
+        Render.gls[thisIndex].compileShader(vShader);
+        if (!Render.gls[thisIndex].getShaderParameter(vShader, Render.gls[thisIndex].COMPILE_STATUS)) {
+          throw 'could not compile shader:' + Render.gls[thisIndex].getShaderInfoLog(vShader);
+        }
 
-      let fShader = Render.gls[thisIndex].createShader(Render.gls[thisIndex].FRAGMENT_SHADER);
-      Render.gls[thisIndex].shaderSource(fShader, Render.fragmentShaderCode);
-      Render.gls[thisIndex].compileShader(fShader);
-      if (!Render.gls[thisIndex].getShaderParameter(fShader, Render.gls[thisIndex].COMPILE_STATUS)) {
-        throw 'could not compile shader:' + Render.gls[thisIndex].getShaderInfoLog(fShader);
-      }
+        let fShader = Render.gls[thisIndex].createShader(Render.gls[thisIndex].FRAGMENT_SHADER);
+        Render.gls[thisIndex].shaderSource(fShader, Render.fragmentShaderCode);
+        Render.gls[thisIndex].compileShader(fShader);
+        if (!Render.gls[thisIndex].getShaderParameter(fShader, Render.gls[thisIndex].COMPILE_STATUS)) {
+          throw 'could not compile shader:' + Render.gls[thisIndex].getShaderInfoLog(fShader);
+        }
 
-      Render.materialProgs[thisIndex] = Render.gls[thisIndex].createProgram();
-      let thisProg = Render.materialProgs[thisIndex];
-      Render.gls[thisIndex].attachShader(thisProg, vShader);
-      Render.gls[thisIndex].attachShader(thisProg, fShader);
-      Render.gls[thisIndex].linkProgram(thisProg);
-      if (!Render.gls[thisIndex].getProgramParameter(thisProg, Render.gls[thisIndex].LINK_STATUS)) {
-        throw ('program filed to link:' + Render.gls[thisIndex].getProgramInfoLog(thisProg));
-      }
+        Render.materialProgs[thisIndex] = Render.gls[thisIndex].createProgram();
+        let thisProg = Render.materialProgs[thisIndex];
+        Render.gls[thisIndex].attachShader(thisProg, vShader);
+        Render.gls[thisIndex].attachShader(thisProg, fShader);
+        Render.gls[thisIndex].linkProgram(thisProg);
+        if (!Render.gls[thisIndex].getProgramParameter(thisProg, Render.gls[thisIndex].LINK_STATUS)) {
+          throw ('program filed to link:' + Render.gls[thisIndex].getProgramInfoLog(thisProg));
+        }
 
-      Render.gls[thisIndex].useProgram(thisProg);
+        Render.gls[thisIndex].useProgram(thisProg);
 
-      Render.materialPosAttrs[thisIndex] = Render.gls[thisIndex].getAttribLocation(thisProg, 'aVertexPosition');
-      Render.materialUVAttrs[thisIndex] = Render.gls[thisIndex].getAttribLocation(thisProg, 'aTextureCoord');
-      Render.gls[thisIndex].enableVertexAttribArray(Render.materialPosAttrs[thisIndex]);
-      Render.gls[thisIndex].enableVertexAttribArray(Render.materialUVAttrs[thisIndex]);
-      Render.gls[thisIndex].uniform1i(Render.gls[thisIndex].getUniformLocation(thisProg, 'uSampler'), 0);
-      Render.updateProjectionMatrix(thisIndex);
-      Render.setViewMatrix(thisIndex, Render.lookAt(0.0, 0.0, thisCanvas.width / 2,
-        0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0));
+        Render.materialPosAttrs[thisIndex] = Render.gls[thisIndex].getAttribLocation(thisProg, 'aVertexPosition');
+        Render.materialUVAttrs[thisIndex] = Render.gls[thisIndex].getAttribLocation(thisProg, 'aTextureCoord');
+        Render.gls[thisIndex].enableVertexAttribArray(Render.materialPosAttrs[thisIndex]);
+        Render.gls[thisIndex].enableVertexAttribArray(Render.materialUVAttrs[thisIndex]);
+        Render.gls[thisIndex].uniform1i(Render.gls[thisIndex].getUniformLocation(thisProg, 'uSampler'), 0);
+        Render.updateProjectionMatrix(thisIndex);
+        Render.setViewMatrix(thisIndex, Render.lookAt(0.0, 0.0, thisCanvas.width / 2,
+          0.0, 0.0, 0.0,
+          0.0, 1.0, 0.0));
 
-      Render.recreateSharedTexture(thisIndex).then(() => {
-        resolve(thisIndex)
+        Render.recreateSharedTexture(thisIndex).then(() => {
+          resolve(thisIndex)
+        })
       })
     })
   }
 
-  // dup methods
+  // call duplication methods
   static setCanvasToUseView(canvasIndex, id) {
     return new Promise(resolve => {
       Render.getSharedTextureSharedHandle(canvasIndex).then(sharedHandle => {
-        console.log(canvasIndex, Render._type[canvasIndex], id, sharedHandle)
         exec('NewWindow', `texture_${canvasIndex}`, `${Render._type[canvasIndex]}:${id},1,1&d3dhandle:${sharedHandle}`)
         .then(res => {
           resolve(res)
         })
       })
     })
-
   }
 
+
+  // start/stop rendering
   static startStopRender(canvasIndex, shouldRender?) {
     return new Promise((resolve, reject) => {
       if (shouldRender === 'undefined') {
@@ -134,7 +138,7 @@ export class Render {
       if (canvasIndex instanceof HTMLCanvasElement) {
         canvasIndex = Render.canvases.indexOf(canvasIndex)
       }
-      if (Render.canvases[canvasIndex]) {
+      if (!Render.canvases[canvasIndex]) {
         reject(Error('Provided canvas could not be found.'))
       }
       Render._isRunning[canvasIndex] = shouldRender;
@@ -191,16 +195,19 @@ export class Render {
     Render.gls[canvasIndex].drawArrays(Render.gls[canvasIndex].TRIANGLE_STRIP, 0, Render.modelVertCount);
   }
 
+  // initialize a web gl context from a canvas
   static initWebGL(canvasIndex) {
-    let gl = null;
-    try {
-      gl = Render.canvases[canvasIndex].getContext('webgl', { preserveDrawingBuffer: true }) || Render.canvases[canvasIndex].getContext('experimental-webgl', { preserveDrawingBuffer: true });
-    } catch (e) { }
-    if (!gl) {
-      alert('Unable to initialize WebGL. Your browser may not support it.');
-      gl = null;
-    }
-    return gl;
+    return new Promise(resolve => {
+      let gl = null;
+      try {
+        gl = Render.canvases[canvasIndex].getContext('webgl', { preserveDrawingBuffer: true }) || Render.canvases[canvasIndex].getContext('experimental-webgl', { preserveDrawingBuffer: true });
+      } catch (e) { }
+      if (!gl) {
+        alert('Unable to initialize WebGL. Your browser may not support it.');
+        gl = null;
+      }
+      resolve(gl);
+    })
   }
 
   static updateProjectionMatrix(canvasIndex) {
