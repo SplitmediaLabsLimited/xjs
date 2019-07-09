9d02e78c12db77d3cfb1ddd51e6a5bf0639db033
Daniel Cefram Ramirez <dcefram@gmail.com>
2019-07-09 20:53:29 +0800

    wip: experiment esm and umd builds

--- src/internal/internal.ts
+++ src/internal/internal.ts
@@ -1,9 +1,12 @@
+// With sample usage of an external dependency: lodash
+import get from 'lodash/get';
 let _callbacks = {};
 
 export function exec(fn: string, ...args: any[]): Promise<any> {
   return new Promise((resolve, reject) => {
     // @TODO: Add condition for remote thingy
-    if (!window.external || !window.external[fn] || typeof window.external !== "function") {
+    const externalFunction = get(window, ['external', fn]);
+    if (typeof externalFunction !== 'function') {
       reject(new Error(`${fn} is not a valid external call, or is not supported on the target environment.`));
       return;
     }
