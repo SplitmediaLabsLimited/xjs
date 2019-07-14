2680ba2d4602a7f1121b8c6839b0400647f3d360
Daniel Cefram Ramirez <dan@daeram.com>
2019-07-15 06:10:51 +0800

    chore: add version helper tool

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-// This is just a test file to figure out if we need rollup or using tsc is already enough
-var internal_1 = require("./internal/internal");
-exports.exec = internal_1.exec;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 // This is just a test file to figure out if we need rollup or using tsc is already enough
 export { exec } from './internal/internal';
+export const version = '%XJS_VERSION%';
