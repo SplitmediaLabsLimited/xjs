8c842716e9ea2cefe35ca179f4d7f3ea4ff695d2
Daniel Cefram Ramirez <dan@daeram.com>
2019-08-13 23:49:24 +0800

    wip: experiments with docs

---
+++ APIREADME.md
@@ -0,0 +1,3 @@
+Welcome to the XJS Framework API reference documentation.
+
+XJS Framework is the development framework, or SDK, for building awesome XSplit Broadcaster Plugins using the popular JavaScript language.
--- src/core/xjs/xjs.ts
+++ src/core/xjs/xjs.ts
@@ -3,31 +3,53 @@ import Internal from 'internal';
 import View from '../view';
 
 /**
- * The Xjs class
+ * # XJS Class
+ *
+ * The entry class that unlocks just about everything that you
+ * can do in XJS Framework.
+ *
+ * You need to control an item in a scene? You'll have to go through
+ * this class. Want to programmatically modify the scene's name?
+ * You'll need to go through this class.
+ *
+ * To use this library using the recommended way (through npm/yarn),
+ * you'll need to import this first.
+ *
+ * ```javascript
+ * import Xjs from 'xjs-framework';
+ * ```
  */
 export default class Xjs {
+  /**
+   * The version number of Xjs
+   */
   static version = '%XJS_VERSION%';
 
   private _internal: Internal;
 
-  type: XjsTypes;
+  private type: XjsTypes;
 
-  environment: XjsEnvironments;
+  private environment: XjsEnvironments;
 
-  logVerbosity: LogVerbosity;
+  private logVerbosity: LogVerbosity;
 
-  version: string;
+  private version: string;
 
-  sendMessage: any;
+  private sendMessage: any;
 
-  onMessageReceive: any;
+  private onMessageReceive: any;
 
-  logger: any;
+  private logger: any;
 
-  exec: any;
+  private exec: any;
 
-  view: View;
-
+  /**
+   * XJS Constructor
+   *
+   * Create an instance of Xjs
+   *
+   * @param config  Config
+   */
   constructor(config: Config = { type: XjsTypes.Local }) {
     Object.keys(config).forEach((key: string) => {
       if (this.hasOwnProperty(key)) {
@@ -41,6 +63,17 @@ export default class Xjs {
     this.exec = this._internal.exec;
   }
 
+  /**
+   * Get an instance of a view based on the index.
+   *
+   * The usual "main" view that is used by both the stream and the XSplit Broadcaster Stage
+   * is `0`.
+   *
+   * If you want to control an item in, let's say, the projector, you'll have to
+   * access a view with a greater index.
+   *
+   * @param index Number
+   */
   getView(index: number) {
     return new View({
       internal: this._internal,
