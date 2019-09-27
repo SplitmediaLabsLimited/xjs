5e931756dc7b8192037d1cbdc64b0fd152772dc3
Daniel Cefram Ramirez <me@danielcefram.com>
2019-09-27 19:04:08 +0800

    feat: expose item props and app props to xjs entry file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // This is just a test file to figure out if we need rollup or using tsc is already enough
 export { default } from './core/xjs';
-export * from './props/item-props';
-export * from './props/app-props';
+export { default as ItemProps } from './props/item-props';
+export { default as AppProps } from './props/app-props';
 export { default as Scene } from './core/scene';
 export { default as Item } from './core/item';
