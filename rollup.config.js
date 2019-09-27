fc231be0789ceefe1cae832d8d6680e16f6cbc0d
Daniel Cefram Ramirez <dcefram@outlook.com>
2019-09-28 02:39:26 +0800

    chore: add terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.ts',
@@ -10,6 +11,7 @@ export default {
       mainFields: ['lodash-es'],
     }),
     commonjs(),
+    terser(),
   ],
   output: {
     file: 'dist/xjs.umd.js',
