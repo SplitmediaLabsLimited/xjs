b0218a892811d1e1aa0860431fd9efad2f66e072
SML_MeSo <meso@splitmedialabs.com>
2020-01-10 17:38:52 +0800

    chore: build

--- dist/xjs.js
+++ dist/xjs.js
@@ -1,3 +1,45 @@
+/**
+ * XSplit JS Framework
+ * version: 2.9.1
+ *
+ * XSplit Extensibility Framework and Plugin License
+ *
+ * Copyright (c) 2015, SplitmediaLabs Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source, minified or binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in minified or binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. This software, in source, minified and binary forms, and any derivatives
+ *    hereof, may be used only with the purpose to extend the functionality of the
+ *    XSplit products, developed and published by SplitmediaLabs Limited. It may
+ *    specifically not be used for extending the functionality of any other software
+ *    products which enables live streaming and/or recording functions.
+ *
+ * 4. This software may not be used to circumvent paid feature restrictions for
+ *    free and personal licensees of the XSplit products.
+ *
+ * THIS SOFTWARE IS PROVIDED BY SPLITMEDIALABS LIMITED ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL SPLITMEDIALABS LIMITED BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ *
+ */
+
+
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /// <reference path="../../defs/es6-promise.d.ts" />
 var app_1 = require('../internal/app');
@@ -4940,8 +4982,8 @@ var Output = (function () {
                 }
                 Output._proxyCallback[name ? callbackName : Output._id] = callback;
                 name ?
-                    internal_1.exec('CallHost', 'getBroadcastChannelXml:' + id, name) :
-                    internal_1.exec('CallHost', 'getBroadcastChannelList:' + id);
+                    internal_1.exec('CallHostFunc', 'getBroadcastChannelXml', name, window_1.default.SetBroadcastChannelXml) :
+                    internal_1.exec('CallHostFunc', 'getBroadcastChannelList', window_1.default.SetBroadcastChannelList);
             }
             else {
                 if (Output._callback[name ? callbackName : Output._id] === undefined) {
@@ -4949,8 +4991,8 @@ var Output = (function () {
                 }
                 Output._callback[name ? callbackName : Output._id] = ({ resolve: resolve });
                 name ?
-                    internal_1.exec('CallHost', 'getBroadcastChannelXml:' + id, name) :
-                    internal_1.exec('CallHost', 'getBroadcastChannelList:' + id);
+                    internal_1.exec('CallHostFunc', 'getBroadcastChannelXml', name, window_1.default.SetBroadcastChannelXml) :
+                    internal_1.exec('CallHostFunc', 'getBroadcastChannelList', window_1.default.SetBroadcastChannelList);
             }
         });
     };
@@ -11051,8 +11093,14 @@ window_1.default.OnAsyncCallback = function (asyncID, result) {
     else {
         var callback = _callbacks[asyncID];
         if (callback instanceof Function) {
-            callback(decodeURIComponent(result));
-            delete _callbacks[asyncID];
+            try {
+                callback(decodeURIComponent(result));
+                delete _callbacks[asyncID];
+            }
+            catch (e) {
+                callback(result);
+                delete _callbacks[asyncID];
+            }
         }
     }
     if (typeof asyncCallback === 'function') {
@@ -11141,13 +11189,11 @@ var Item = (function () {
                             app_1.App.getAsItemList('presetconfig')
                                 .then(function (jsonArr) {
                                 for (var i = 0; i < jsonArr.length; i++) {
-                                    if (jsonArr[i].children !== undefined) {
-                                        for (var j = 0; j < jsonArr[i].children.length; j++) {
-                                            if (jsonArr[i].children[j]['srcid'] === srcId) {
-                                                sceneMatch = i;
-                                                idMatch = jsonArr[i].children[j]['id'];
-                                                break;
-                                            }
+                                    if (jsonArr[i] !== undefined) {
+                                        if (jsonArr[i]['srcid'] === srcId) {
+                                            sceneMatch = i;
+                                            idMatch = jsonArr[i]['id'];
+                                            break;
                                         }
                                     }
                                     if (idMatch !== undefined) {
@@ -11256,13 +11302,11 @@ var Item = (function () {
                             app_1.App.getAsItemList('presetconfig')
                                 .then(function (jsonArr) {
                                 for (var i = 0; i < jsonArr.length; i++) {
-                                    if (jsonArr[i].children !== undefined) {
-                                        for (var j = 0; j < jsonArr[i].children.length; j++) {
-                                            if (jsonArr[i].children[j]['srcid'] === srcId) {
-                                                sceneMatch = i;
-                                                idMatch = jsonArr[i].children[j]['id'];
-                                                break;
-                                            }
+                                    if (jsonArr[i] !== undefined) {
+                                        if (jsonArr[i]['srcid'] === srcId) {
+                                            sceneMatch = i;
+                                            idMatch = jsonArr[i]['id'];
+                                            break;
                                         }
                                     }
                                     if (idMatch !== undefined) {
